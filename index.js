b4d30802447ab9a8da75dd811aa22c77faa12451
Vinícius do Carmo <vinicius.m.do.carmo@gmail.com>
2015-06-26 10:40:24 -0300

    Operates the middle index just one time.

--- index.js
+++ index.js
@@ -7,12 +7,13 @@ module.exports = function peakFinding(list) {
 		throw new TypeError('Expected an Array');
 	}
 
-	var len = list.length;
+	var len = list.length, 
+	    middleIndex = floor(len/2);
 
-	if(list[floor(len/2)] < list[floor(len/2 - 1)])
-		return peakFinding(list.slice(0, floor(len/2)));
-	else if(list[floor(len/2)] < list[floor(len/2 + 1)])
-		return peakFinding(list.slice(len/2 + 1, len - 1));
+	if(list[middleIndex] < list[middleIndex + 1])
+		return peakFinding(list.slice(0, middleIndex));
+	else if(list[middleIndex] < list[middleIndex + 1])
+		return peakFinding(list.slice(middleIndex + 1, len - 1));
     else
-		return list[floor(len/2)];
+		return list[middleIndex];
 };
