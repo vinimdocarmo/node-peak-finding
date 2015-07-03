1481d88f6101338cc41f5a3d7a5e07fc780dbbeb
Vinícius do Carmo <vinicius.m.do.carmo@gmail.com>
2015-07-03 01:05:31 -0300

    Change the recursive approach to the iteractive one. closes #4

--- src/peak-finding.js
+++ src/peak-finding.js
@@ -3,17 +3,23 @@
 var floor = Math.floor;
 
 module.exports = function peakFinding(list) {
-	if (!Array.isArray(list)) {
+	if(!Array.isArray(list)) {
 		throw new TypeError('Expected an Array');
 	}
 
 	var len = list.length,
+		firstIndex = 0,
+		lastIndex = len - 1,
 	    middleIndex = floor(len/2);
 
-	if(list[middleIndex] < list[middleIndex - 1])
-		return peakFinding(list.slice(0, middleIndex));
-	else if(list[middleIndex] < list[middleIndex + 1])
-		return peakFinding(list.slice(middleIndex + 1, len));
-    else
-		return list[middleIndex];
+	while(firstIndex <= lastIndex) {
+		middleIndex = floor((firstIndex + lastIndex) / 2);
+
+		if(list[middleIndex] < list[middleIndex - 1])
+			lastIndex = middleIndex - 1;
+		else if(list[middleIndex] < list[middleIndex + 1])
+			firstIndex = middleIndex + 1;
+		else
+			return list[middleIndex];
+	}
 };
