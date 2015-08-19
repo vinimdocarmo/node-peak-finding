bd8cf70e7c3a3b9b12db13de4919adb76e0c8f2f
Vinícius do Carmo <vinicius.m.do.carmo@gmail.com>
2015-08-18 22:51:04 -0300

    Implement 2D version

--- src/peak-finding.js
+++ src/peak-finding.js
@@ -2,32 +2,64 @@
 
 var floor = Math.floor;
 
-module.exports = function peakFinding(list, opts) {
-	if(!Array.isArray(list)) {
+/**
+ *
+ * @param {Array} array - An array of number or a matrix (array of arrays)
+ * @param {Object} opts
+ * @param {Boolean} opts.highest - Whether to return the highest peak (only in one dimension version)
+ * @param {Boolean} opts.twoDimension - Whether it is the two dimension version
+ *
+ * @returns {Number}
+ */
+module.exports = function peakFinding(array, opts) {
+	if(!Array.isArray(array)) {
 		throw new TypeError('Expected an Array');
 	}
 
 	opts = opts || {};
 	opts.highest = opts.highest || false;
+	opts.twoDimension = opts.twoDimension || false;
 
-	if(opts.highest) {
-		return Math.max.apply(Math, list);
+	if(opts.highest && !opts.twoDimension) {
+		return Math.max.apply(Math, array);
 	}
 
-	var len = list.length,
+	var len = array.length,
 		firstIndex = 0,
 		lastIndex = len - 1,
 	    middleIndex = floor(len/2);
 
 	while(firstIndex <= lastIndex) {
+		var currEl, leftEl, rightEl;
+
 		middleIndex = floor((firstIndex + lastIndex) / 2);
 
-		if(list[middleIndex] < list[middleIndex - 1]) {
+		if(opts.twoDimension) {
+			var max = array[0][middleIndex],
+				rowIndex = 0;
+
+			for(var i = 1; i < len; i++) {
+				if(array[i][middleIndex] > max) {
+					max = array[i][middleIndex];
+					rowIndex = i;
+				}
+			}
+
+			currEl  = array[rowIndex][middleIndex];
+			leftEl  = array[rowIndex][middleIndex - 1];
+			rightEl = array[rowIndex][middleIndex + 1];
+		} else {
+			currEl  = array[middleIndex];
+			leftEl  = array[middleIndex - 1];
+			rightEl = array[middleIndex + 1];
+		}
+
+		if(currEl < leftEl) {
 			lastIndex = middleIndex - 1;
-		} else if(list[middleIndex] < list[middleIndex + 1]) {
+		} else if(currEl < rightEl) {
 			firstIndex = middleIndex + 1;
 		} else {
-			return list[middleIndex];
+			return currEl;
 		}
 	}
 };
